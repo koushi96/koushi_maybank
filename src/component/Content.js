2cab0003ea5f7d6f98c173e782e20cb62bbabdbc
koushi96 <77238317+koushi96@users.noreply.github.com>
2022-08-31 09:06:27 +0800

    full interface

--- src/component/Content.js
+++ src/component/Content.js
@@ -1,12 +1,93 @@
 import React from "react";
-import product_card from "../data/product_data";
-// import { Navigate } from "react-router-dom";
+import ProductListing from "./ProductListing";
+import { QuantityPicker } from 'react-qty-picker';
+const product_card = [
+    {
+        id:1,
+        product_name:"Alpha Arbutin",
+        description: "Pigmention and uneven skintone",
+        price: 67,
+        currency: "MYR",
+        thumb: "./images/1.jpg",
+        stock: 22
+    },
+    {
+        id:2,
+        product_name:"Niacinamide",
+        description: "Pigmention and uneven skintone",
+        price: 87,
+        currency: "MYR",
+        thumb: "./images/2.jpg",
+        stock: 10
+    },
+    {
+        id:3,
+        product_name:"Retinol",
+        description: "Pigmention and uneven skintone",
+        price: 99,
+        currency: "MYR",
+        thumb: "./images/3.jpg",
+        stock: 31
+    },
+    {
+        id:4,
+        product_name:"AHA",
+        description: "Pigmention and uneven skintone",
+        price: 82,
+        currency: "MYR",
+        thumb: "./images/4.jpg",
+        stock: 18
+    },
+    {
+        id:5,
+        product_name:"Squalane",
+        description: "Pigmention and uneven skintone",
+        price: 55,
+        currency: "MYR",
+        thumb: "./images/5.jpg",
+        stock: 12
+    },
+    {
+        id:6,
+        product_name:"Glycol",
+        description: "Pigmention and uneven skintone",
+        price: 99,
+        currency: "MYR",
+        thumb: "./images/6.jpg",
+        stock: 25
+    }
 
+]
 
+const Content = () => {
+    const [goToProduct, setGotoProduct] = React.useState(false);
+    var productList = [];
+    var productList2 = [];
+    const [prodDetail, setProdDetail] = React.useState([]);
+    const [productStock, setProductStock] = React.useState();
+    const [items, setItems] = React.useState(product_card);
 
-const handleClick = function (itemId : number){
-        var productList = [];
-        var productList2 = [];
+    function handleChange(newValue) {
+        setProductStock(newValue)
+        console.log(productStock)
+        console.log(items)
+    }
+
+    const handleClickNewStock = function (e, stock : number, id : number){
+        console.log("stock : ",stock)
+        console.log("id", id)
+        console.log("test : ", product_card[id-1].product_name)
+        if (stock !== 0){
+            product_card[id-1].stock = stock
+            console.log("edited : ", product_card[id-1].stock)
+        } else {
+            setItems((items) => items.filter((_, i) => i !== id));
+            console.log("delete")
+        }
+        
+    }
+    
+    const handleClick = function (e, itemId : number){
 
         Object.keys(product_card).forEach(function(itemId) {
             productList.push(<option value={itemId}>{product_card[itemId]}</option>);
@@ -20,27 +101,28 @@ const handleClick = function (itemId : number){
         })
 
         productList2.forEach((data)=>{
-            console.log(data)
+            prodDetail.push(data)
         })
 
+        setProdDetail(prodDetail)
         
-
     }
 
-// const Redirect = () => {
-//     window.open("./ProductListing.js");
-// }
 
+    if (goToProduct) {
+        return (
+            <div className="container">
+            <ProductListing prodDetail={prodDetail}/>
+            </div>
+        );
+    }
 
-
-
-
-const Content = () => {
-    
-    console.log(product_card);
     const listItems = product_card.map((item) => 
-        <div className="card" key={item.id}>
-            <div className="card_img">
+        <div className="card" key={item.id} >
+            <div className="card_img" onClick = {(e : any) =>
+                [handleClick(e, item.id),
+                setGotoProduct(true)]
+            }>
                 <img src={item.thumb} alt={item.imgdsc}/> 
             </div>
             
@@ -48,16 +130,21 @@ const Content = () => {
                 <h2>{item.product_name}</h2>
                 <p>{item.description}</p>
                 <p className="price">{item.price}<span>{item.currency}</span></p>
-                {/* <a href="./ProductListing" >
-                    Link
-                </a> */}
-                <button onClick = {(e : any) =>
-                    [handleClick(e, item.id), 
-                        // Redirect()
-                    ]
+                {/* <div className="btn" onClick = {(e : any) =>
+                    [handleClick(e, item.id),
+                    setGotoProduct(true)]
                 }>
                     {item.stock}
-                </button>
+                </div> */}
+                <QuantityPicker min={0} max={item.stock} value={item.stock} 
+                onChange={(value)=>{
+                    handleChange(value); 
+                }}/>
+                <div className="btn" onClick = {(e : any) =>
+                    handleClickNewStock(e, productStock ,item.id)
+                }>
+                    Set New Quantity
+                </div>
             </div>
         </div>
 
@@ -72,4 +159,3 @@ const Content = () => {
 }
 export default Content;
 
-//https://www.youtube.com/watch?v=i0rbmzyaVaM
--- src/component/ProductListing.js
+++ src/component/ProductListing.js
@@ -1,29 +1,57 @@
 import React from "react";
-// import { connect } from "react-redux";
-import { QuantityPicker } from 'react-qty-picker';
+// import { QuantityPicker } from 'react-qty-picker';
 
 
-const Content = () => {
+const ProductListing = (prodDetail : any) => {
+    // let value = prodDetail.prodDetail[6];
+    // let id = prodDetail.prodDetail[0];
+    // const [productStock, setProductStock] = React.useState(value)
+    // const [list, setList] = React.useState(product_card);
+
+    
+    // function handleChange(newValue) {
+    //     setProductStock(newValue)
+    // }
+
+    // const handleClick = function (e, stock : number, id : number){
+    //     console.log("stock : ",stock)
+    //     console.log("id", id)
+    //     // console.log("test : ", product_card[id-1].product_name)
+    //     // if (stock !== 0){
+    //     //     product_card[id-1].stock = stock
+    //     //     console.log("edited : ", product_card[id-1].stock)
+    //     // } else {
+    //     //     console.log("delete")
+    //     // }
+        
+    // }
 
 return(
         <div className="product_content">
-            
             
             <h3>Glow Saloon</h3>
             <div className="spaceND"></div>
             <div className="card" key={2}>
             <div className="card_img_2">
-                <img src="./images/1.jpg" alt="imgdsc"/> 
+                <img src={prodDetail.prodDetail[5]} alt="imgdsc"/> 
             </div>
             
             <div className="card_header">
-                <h2>Retinol</h2>
-                <p>Pigmention and uneven skintone</p>
-                <p className="price">123<span>MYR</span></p>
-                <QuantityPicker min={0} max={10} value={10}/>
+                <h2>{prodDetail.prodDetail[1]}</h2>
+                <p>{prodDetail.prodDetail[2]}</p>
+                <p className="price">{prodDetail.prodDetail[3]}<span>{prodDetail.prodDetail[4]}</span></p>
+                {/* <QuantityPicker min={0} max={value} value={prodDetail.prodDetail[6]} 
+                onChange={(value)=>{
+                    handleChange(value); 
+                }}/>
+                <div className="btn" onClick = {(e : any) =>
+                    handleClick(e, productStock ,id)
+                }>
+                    Set New Quantity
+                </div> */}
             </div>
         </div>
         </div>
     )
 }
-export default Content;
+export default ProductListing;
